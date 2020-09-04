900fb8ac4abb1ceaf21ae9dd63419df678c91af7
Steve Flenniken <steve.flenniken@gmail.com>
2020-09-04 12:45:17 -0700

    Simplify tests

--- tests/test_parseCommandLine.nim
+++ tests/test_parseCommandLine.nim
@@ -4,19 +4,29 @@ import unittest
 import parseCommandLine
 import streams
 
-
 proc readLines(stream: Stream): seq[string] =
   stream.setPosition(0)
   for line in stream.lines():
     result.add line
 
-
-proc parseCmdLine(cmdLine: string): tuple[args: Args, warningLines: seq[string]] =
+proc tpcl(cmdLine: string, version: bool=false, help: bool=false, serverList: seq[string] = @[],
+  sharedList: seq[string] = @[], templateList: seq[string] = @[], resultList: seq[string] = @[],
+  warningLines: seq[string] = @[]) =
+  
   var stream = newStringStream()
   defer: stream.close()
+
   let args = parseCommandLine(stream, cmdLine)
-  let warningLines = stream.readLines()
-  result = (args, warningLines)
+  let lines = stream.readLines()
+
+  check(args.version == version)
+  check(args.help == help)
+  check(args.filenames[0] == serverList)
+  check(args.filenames[1] == sharedList)
+  check(args.filenames[2] == templateList)
+  check(args.filenames[3] == resultList)
+  check(lines == warningLines)
+
 
 
 # statictea --server=server.json --sharedJson=shared.json --template=template.html --result=result.htm
@@ -43,152 +53,52 @@ suite "Test statictea.nim":
     check(warningLines[1] == "1 2 3")
 
   test "parseCommandLine-v":
-    var (args, lines) = parseCmdLine("-v")
-    check(lines.len == 0)
-    check(args.help == false)
-    check(args.version == true)
-    check(args.filenames[0].len == 0)
-    check(args.filenames[1].len == 0)
-    check(args.filenames[2].len == 0)
-    check(args.filenames[3].len == 0)
+    tpcl("-v", version=true)
 
   test "parseCommandLine-h":
-    var (args, lines) = parseCmdLine("-h")
-    check(lines.len == 0)
-    check(args.help == true)
-    check(args.version == false)
-    check(args.filenames[0].len == 0)
-    check(args.filenames[1].len == 0)
-    check(args.filenames[2].len == 0)
-    check(args.filenames[3].len == 0)
+    tpcl("-h", help=true)
 
   test "parseCommandLine-t":
-    var (args, lines) = parseCmdLine("-t=tea.html")
-    check(lines.len == 0)
-    check(args.help == false)
-    check(args.version == false)
-    check(args.filenames[0].len == 0)
-    check(args.filenames[1].len == 0)
-    check(args.filenames[2].len == 1)
-    check(args.filenames[2][0] == "tea.html")
-    check(args.filenames[3].len == 0)
+    tpcl("-t=tea.html", templateList = @["tea.html"])
 
   test "parseCommandLine-template":
-    var (args, lines) = parseCmdLine("--template=tea.html")
-    check(lines.len == 0)
-    check(args.help == false)
-    check(args.version == false)
-    check(args.filenames[0].len == 0)
-    check(args.filenames[1].len == 0)
-    check(args.filenames[2].len == 1)
-    check(args.filenames[2][0] == "tea.html")
-    check(args.filenames[3].len == 0)
+    tpcl("--template=tea.html", templateList = @["tea.html"])
 
   test "parseCommandLine-s":
-    var (args, lines) = parseCmdLine("-s=server.json")
-    check(lines.len == 0)
-    check(args.help == false)
-    check(args.version == false)
-    check(args.filenames[0].len == 1)
-    check(args.filenames[0][0] == "server.json")
-    check(args.filenames[1].len == 0)
-    check(args.filenames[2].len == 0)
-    check(args.filenames[3].len == 0)
+    tpcl("-s=server.json", serverList = @["server.json"])
 
   test "parseCommandLine-server":
-    var (args, lines) = parseCmdLine("--server=server.json")
-    check(lines.len == 0)
-    check(args.help == false)
-    check(args.version == false)
-    check(args.filenames[0].len == 1)
-    check(args.filenames[0][0] == "server.json")
-    check(args.filenames[1].len == 0)
-    check(args.filenames[2].len == 0)
-    check(args.filenames[3].len == 0)
+    tpcl("--server=server.json", serverList = @["server.json"])
 
   test "parseCommandLine-j":
-    var (args, lines) = parseCmdLine("-j=shared.json")
-    check(lines.len == 0)
-    check(args.help == false)
-    check(args.version == false)
-    check(args.filenames[0].len == 0)
-    check(args.filenames[1].len == 1)
-    check(args.filenames[1][0] == "shared.json")
-    check(args.filenames[2].len == 0)
-    check(args.filenames[3].len == 0)
+    tpcl("-j=shared.json", sharedList = @["shared.json"])
 
   test "parseCommandLine-shared":
-    var (args, lines) = parseCmdLine("--shared=shared.json")
-    check(lines.len == 0)
-    check(args.help == false)
-    check(args.version == false)
-    check(args.filenames[0].len == 0)
-    check(args.filenames[1].len == 1)
-    check(args.filenames[1][0] == "shared.json")
-    check(args.filenames[2].len == 0)
-    check(args.filenames[3].len == 0)
+    tpcl("--shared=shared.json", sharedList = @["shared.json"])
 
   test "parseCommandLine-r":
-    var (args, lines) = parseCmdLine("-r=result.html")
-    check(lines.len == 0)
-    check(args.help == false)
-    check(args.version == false)
-    check(args.filenames[0].len == 0)
-    check(args.filenames[1].len == 0)
-    check(args.filenames[2].len == 0)
-    check(args.filenames[3].len == 1)
-    check(args.filenames[3][0] == "result.html")
+    tpcl("-r=result.html", resultList = @["result.html"])
 
   test "parseCommandLine-result":
-    var (args, lines) = parseCmdLine("--result=result.html")
-    check(lines.len == 0)
-    check(args.help == false)
-    check(args.version == false)
-    check(args.filenames[0].len == 0)
-    check(args.filenames[1].len == 0)
-    check(args.filenames[2].len == 0)
-    check(args.filenames[3].len == 1)
-    check(args.filenames[3][0] == "result.html")
+    tpcl("--result=result.html", resultList = @["result.html"])
 
   test "parseCommandLine-happy-path":
-    var (args, lines) = parseCmdLine("-s=server.json -j=shared.json -t=tea.html -r=result.html")
-    check(args.help == false)
-    check(args.version == false)
-    check(args.filenames[0].len == 1)
-    check(args.filenames[0][0] == "server.json")
-    check(args.filenames[1].len == 1)
-    check(args.filenames[1][0] == "shared.json")
-    check(args.filenames[2].len == 1)
-    check(args.filenames[2][0] == "tea.html")
-    check(args.filenames[3].len == 1)
-    check(args.filenames[3][0] == "result.html")
-    check(lines.len == 0)
+    tpcl("-s=server.json -j=shared.json -t=tea.html -r=result.html",
+         serverList = @["server.json"],
+         sharedList = @["shared.json"],
+         templateList = @["tea.html"],
+         resultList = @["result.html"],
+    )
 
   test "parseCommandLine-multiple":
-    var (args, lines) = parseCmdLine("-s=server.json -s=server2.json -j=shared.json -j=shared2.json -t=tea.html -r=result.html")
-    check(lines.len == 0)
-    check(args.help == false)
-    check(args.version == false)
-    check(args.filenames[0].len == 2)
-    check(args.filenames[0][0] == "server.json")
-    check(args.filenames[0][1] == "server2.json")
-    check(args.filenames[1].len == 2)
-    check(args.filenames[1][0] == "shared.json")
-    check(args.filenames[1][1] == "shared2.json")
-    check(args.filenames[2].len == 1)
-    check(args.filenames[2][0] == "tea.html")
-    check(args.filenames[3].len == 1)
-    check(args.filenames[3][0] == "result.html")
+    tpcl("-s=server.json -s=server2.json -j=shared.json -j=shared2.json -t=tea.html -r=result.html",
+         serverList = @["server.json", "server2.json"],
+         sharedList = @["shared.json", "shared2.json"],
+         templateList = @["tea.html"],
+         resultList = @["result.html"],
+    )
 
   # Test some error cases.
 
   test "parseCommandLine-missing-name":
-    var (args, lines) = parseCmdLine("-s")
-    check(args.help == false)
-    check(args.version == false)
-    check(args.filenames[0].len == 0)
-    check(args.filenames[1].len == 0)
-    check(args.filenames[2].len == 0)
-    check(args.filenames[3].len == 0)
-    check(lines.len == 1)
-    check(lines[0] == "warning 4: No server filename. Use s=filename.")
+    tpcl("-s", warningLines = @["warning 4: No server filename. Use s=filename."])
