1b8f61775ecb53316abe00f208a56235e61f1a1f
Steve Flenniken <steve.flenniken@gmail.com>
2020-09-03 21:49:58 -0700

    Parse command line.

--- src/parseCommandLine.nim
+++ src/parseCommandLine.nim
@@ -3,17 +3,35 @@
 import parseopt
 import streams
 import strutils
-import sets
+import tables
+
 
 type
-  Args* = tuple
+  # ListType = enum serverList, sharedList, templateList, resultList
+  Args* = object
     ## Command line arguments.
-    help: bool
-    version: bool
-    serverList: seq[string]
-    sharedList: seq[string]
-    templateList: seq[string]
-    resultList: seq[string]
+    help*: bool
+    version*: bool
+    filenames*: array[4, seq[string]]
+
+
+let wordToListIndex = {
+  "server": 0,
+  "shared": 1,
+  "template": 2,
+  "result": 3,
+}.toTable
+
+
+let letterToWord = {
+  's': "server",
+  'j': "shared",
+  't': "template",
+  'r': "result",
+  'h': "help",
+  'v': "version",
+}.toTable
+
 
 proc `$`*(args: Args): string =
   ## Return a string representation of Args.
@@ -25,40 +43,10 @@ Args:
   sharedList=$4
   templateList=$5
   resultList=$6
-""" % [$args.help, $args.version, $args.serverList, $args.sharedList, $args.templateList, $args.resultList]
+""" % [$args.help, $args.version, $args.filenames[0], $args.filenames[1],
+       $args.filenames[2], $args.filenames[3]]
 
 
-proc addFilename(word: string, filename: string, args: var Args) =
-  ## Add a filename to the args list specified by the word.
-  case word
-  of "server":
-    args.serverList.add(filename)
-  of "shared":
-    args.sharedList.add(filename)
-  of "template":
-    args.templateList.add(filename)
-  of "result":
-    args.resultList.add(filename)
-  else:
-    discard
-
-
-proc letterToWord(letter: char): string =
-  case letter
-  of 's':
-    result = "server"
-  of 'j':
-    result = "shared"
-  of 't':
-    result = "template"
-  of 'r':
-    result = "result"
-  of 'h':
-    result = "help"
-  of 'v':
-    result = "version"
-  else:
-    result = ""
 
 
 proc handleWord(switch: string, word: string, value: string,
@@ -66,12 +54,12 @@ proc handleWord(switch: string, word: string, value: string,
   ## switch is either a word or a letter, the value from the command line.
   ## word is the name in the args object.
 
-  let filenameList = toHashSet(["server", "shared", "template", "result"])
-  if word in filenameList:
+  let listIndex = wordToListIndex.getOrDefault(word, -1)
+  if listIndex != -1:
     if value == "":
       warnings.writeLine("warning 4: No $1 filename. Use $2=filename." % [word, $switch])
     else:
-      addFilename(word, value, args)
+      args.filenames[listIndex].add(value)
   elif word == "help":
     args.help = true
   elif word == "version":
@@ -80,9 +68,10 @@ proc handleWord(switch: string, word: string, value: string,
     warnings.writeLine("warning 3: Unknown switch: $1" % $switch)
 
 
-
 proc parseCommandLine*(warnings: Stream, cmdLine: string=""): Args =
   ## Return the command line parameters and write warnings to the stream.
+
+  # result = Args(help: false, version: false, filenames: [seq[], seq[], seq[], seq[]])
 
   var optParser = initOptParser(cmdLine)
 
@@ -93,7 +82,7 @@ proc parseCommandLine*(warnings: Stream, cmdLine: string=""): Args =
       of CmdLineKind.cmdShortOption:
         for ix in 0..key.len-1:
           let letter = key[ix]
-          let word = letterToWord(letter)
+          let word = letterToWord.getOrDefault(letter, "")
           if word == "":
             warnings.writeLine("warning 3: Unknown switch: $1" % $letter)
           else:
