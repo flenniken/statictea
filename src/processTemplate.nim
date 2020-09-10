4f182d7b1bc6cdd50695a258a52fa51497cb9cfa
Steve Flenniken <steve.flenniken@gmail.com>
2020-09-10 08:13:11 -0700

    Add value object.

--- src/processTemplate.nim
+++ src/processTemplate.nim
@@ -4,6 +4,27 @@ import streams
 import strutils
 import args
 import warnings
+import tables
+
+type
+  ValueKind = enum
+    vkString,
+    vkInt,
+    vkFloat,
+    # vkDictionary, vkList, vkFunction,
+
+  Value = object
+    case kind: ValueKind
+      of vkString:
+        stringValue: string
+      of vkInt:
+        intValue: int
+      of vkFloat:
+        floatValue: float
+
+proc readJson(warn: Stream, filename: string, serverVars: var Table[string, Value]) =
+  warn.writeLine("readJson not implemented")
+
 
 proc processTemplate*(warnings: Stream, args: Args) =
 
@@ -15,5 +36,12 @@ proc processTemplate*(warnings: Stream, args: Args) =
   if args.templateList.len > 1:
     let skipping = join(args.templateList[1..^1], ", ")
     warning(warnings, "starting", 0, wOneTemplateAllowed, skipping)
-# file(line): w1: One template file allowed, skipping: tea2.html, tea3.html"
   echo "processing template"
+
+  var serverVars = initTable[string, Value]()
+  for filename in args.serverList:
+    readJson(warnings, filename, serverVars)
+
+  var sharedVars = initTable[string, Value]()
+  for filename in args.sharedList:
+    readJson(warnings, filename, sharedVars)
