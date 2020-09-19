3ef3fee2473c3c61ab500e07db49c7adb273bd3e
Steve Flenniken <steve.flenniken@gmail.com>
2020-09-19 14:34:20 -0700

    Update warning logger.

--- src/warnLogger.nim
+++ src/warnLogger.nim
@@ -6,6 +6,10 @@ var warnStream: Stream
 var isStderrStream: bool
 
 proc openWarnLog*(stderrStream: bool) =
+  ## Open the warning stream as either the stderr stream or a
+  ## string stream.
+  if warnStream != nil:
+    return
   if stderrStream:
     warnStream = newFileStream(stderr)
   else:
@@ -13,20 +17,25 @@ proc openWarnLog*(stderrStream: bool) =
   isStderrStream = stderrStream
 
 proc closeWarnLog*() =
+  ## Close the warning stream. If not open, do nothing.
   if warnStream != nil:
     warnStream.close()
+    warnStream = nil
 
 proc warn*(message: string) =
-  if warnStream != nil:
+  ## Write to the warning stream. Do nothing when it's not open. If
+  ## there is an io error writing, close the stream.
+  if warnStream == nil:
+    return
+  try:
     warnStream.writeLine(message)
+  except:
+    closeWarnLog()
 
 proc readWarnLines*(): seq[string] =
+  # Read the warning lines from the string stream. Return nothing for
+  # stderr.
   if warnStream != nil and not isStderrStream:
     warnStream.setPosition(0)
     for line in warnStream.lines():
       result.add line
-
-proc clearWarnLog*() =
-  if warnStream != nil:
-    warnStream.close()
-    openWarnLog(isStderrStream)
--- tests/test_warnLogger.nim
+++ tests/test_warnLogger.nim
@@ -3,39 +3,49 @@ import warnLogger
 
 proc someProc() =
   warn("running someProc")
-  warn("asdf")
+  warn("testing warning logger")
   warn("leaving someProc")
 
 suite "Test warnLogger.nim":
 
-  setup:
-    openWarnLog(false)
-
   test "happy path":
-    var lines: seq[string]
+    openWarnLog(false)
     let message = "tea tea tea"
     warn(message)
-
-    lines = readWarnLines()
+    var lines = readWarnLines()
     check lines.len == 1
     check lines[0] == message
+    closeWarnLog()
 
-    clearWarnLog()
+  test "warn from proc":
+    openWarnLog(false)
+    someProc()
+    var lines = readWarnLines()
+    check lines.len == 3
+    closeWarnLog()
+
+  test "open close twice":
+    openWarnLog(false)
+    openWarnLog(false)
+    closeWarnLog()
+    closeWarnLog()
+
+  test "read from stream":
+    var lines = readWarnLines()
+    check lines.len == 0
+
+    openWarnLog(false)
+    someProc()
+    lines = readWarnLines()
+    check lines.len == 3
+    closeWarnLog()
+
     lines = readWarnLines()
     check lines.len == 0
 
+  test "stderr stream":
+    openWarnLog(true)
     someProc()
-
-    lines = readWarnLines()
-    check lines.len == 3
-
-  test "test log":
-    var lines: seq[string]
-    clearWarnLog()
-    lines = readWarnLines()
+    var lines = readWarnLines()
     check lines.len == 0
-
-    someProc()
-
-    lines = readWarnLines()
-    check lines.len == 3
+    closeWarnLog()
