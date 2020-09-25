d9b1fdddd876ad50cc2992924332dfea6143f171
Steve Flenniken <steve.flenniken@gmail.com>
2020-09-24 20:51:14 -0700

    String rep of args.

--- src/args.nim
+++ src/args.nim
@@ -1,6 +1,5 @@
 ## Command line arguments.
 import strutils
-import sets
 
 type
   Prepost* = tuple[pre: string, post: string]
@@ -13,62 +12,20 @@ type
     serverList*: seq[string]
     sharedList*: seq[string]
     templateList*: seq[string]
-    resultFilename*: string
     prepostList*: seq[Prepost]
-
-proc addBool(name: string, value: bool, trueElements: var seq[string],
-             falseElements: var seq[string]) =
-  if value:
-    trueElements.add(name)
-  else:
-    falseElements.add(name)
-
-proc addEmpty(name: string, value: seq[string] | seq[Prepost], emptyElements: var seq[string]) =
-  if value.len == 0:
-    emptyElements.add(name)
-
-proc addEmptyString(name: string, value: string, emptyElements: var seq[string]) =
-  if value == "":
-    emptyElements.add(name)
-
-proc addLine(name: string, elements: seq[string], lines: var seq[string]) =
-  if elements.len != 0:
-    lines.add("$1 = $2" % [name, elements.join(", ")])
-
-proc addLine(name: string, elements: seq[Prepost], lines: var seq[string]) =
-  if elements.len != 0:
-    lines.add("$1 = $2" % [name, elements.join(", ")])
-
-proc addLine(name: string, value: string, lines: var seq[string]) =
-  if value != "":
-    lines.add("$1 = $2" % [name, value])
+    resultFilename*: string
 
 func `$`*(args: Args): string =
   ## A string representation of Args.
-  var trueElements: seq[string]
-  var falseElements: seq[string]
-  var emptyElements: seq[string]
-
-  for name, value in args.fieldPairs:
-    when value is bool:
-      addBool(name, value, trueElements, falseElements)
-    elif value is seq:
-      addEmpty(name, value, emptyElements)
-    elif value is string:
-      addEmptyString(name, value, emptyElements)
-
-  var lines: seq[string]
-  lines.add("args:")
-  lines.add("true: $1" % [join(trueElements, ", ")])
-  lines.add("false: $1" % [join(falseElements, ", ")])
-  lines.add("empty: $1" % [join(emptyElements, ", ")])
-
-  for name, value in args.fieldPairs:
-    when value is seq[string]:
-      addLine(name, value, lines)
-    elif value is seq[Prepost]:
-      addLine(name, value, lines)
-    elif value is string:
-      addLine(name, value, lines)
-
-  result = join(lines, "\n")
+  result = """
+Args:
+help=$1, version=$2, update=$3, log=$4
+serverList: [$5]
+sharedList: [$6]
+templateList: [$7]
+prepostList: [$8]
+resultFilename: "$9"
+""" % [$args.help, $args.version, $args.update, $args.log,
+  $args.serverList.join(", "), $args.sharedList.join(", "),
+  $args.templateList.join(", "), $args.prepostList.join(", "),
+  $args.resultFilename]
--- tests/test_args.nim
+++ tests/test_args.nim
@@ -1,32 +1,39 @@
 import unittest
 import args
-import strutils
 
 suite "test_args.nim":
 
-  test "fields":
+  test "empty args":
     var args: Args
-    for name, value in args.fieldPairs:
-      echo "$1 = $2" % [name, $value]
+    let expected = """
+Args:
+help=false, version=false, update=false, log=false
+serverList: []
+sharedList: []
+templateList: []
+prepostList: []
+resultFilename: ""
+"""
+    check $args == expected
 
-  test "show":
+  test "args string":
     var args: Args
     args.help = true
     args.serverList = @["one.json", "two.json"]
     args.resultFilename = "result.html"
     args.prepostList = @[("#", "@"), ("begin", "end")]
-    echo $args
-
-  test "args to string":
-    var args: Args
     let expected = """
 Args:
-false: help, version, update, log
-empty: serverList, sharedList, templateList, resultFilename, prepostList
+help=true, version=false, update=false, log=false
+serverList: [one.json, two.json]
+sharedList: []
+templateList: []
+prepostList: [(pre: "#", post: "@"), (pre: "begin", post: "end")]
+resultFilename: "result.html"
 """
-    check($args == expected)
+    check $args == expected
 
-  test "args to string2":
+  test "args string2":
     var args: Args
     args.help = true
     args.serverList = @["server.json", "more.json"]
@@ -34,11 +41,11 @@ empty: serverList, sharedList, templateList, resultFilename, prepostList
     args.resultFilename = "result.html"
     let expected = """
 Args:
-true: help
-false: version, update, log
-empty: templateList, prepostList
-serverList=server.json, more.json
-sharedList=shared.json
-resultFilename=result.html
+help=true, version=false, update=false, log=false
+serverList: [server.json, more.json]
+sharedList: [shared.json]
+templateList: []
+prepostList: []
+resultFilename: "result.html"
 """
     check($args == expected)
