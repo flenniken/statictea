42f99dcd2fc5d1e169995757b0e9aec30d9a1f6f
Steve Flenniken <steve.flenniken@gmail.com>
2020-10-01 08:06:42 -0700

    Add main tests.

--- tests/test_statictea.nim
+++ tests/test_statictea.nim
@@ -2,6 +2,7 @@ import unittest
 import statictea
 import version
 import streams
+import strutils
 
 proc readAndClose(stream: Stream): seq[string] =
   ## Read the stream's lines then close it.
@@ -19,3 +20,26 @@ suite "Test statictea.nim":
     let output = readAndClose(stream)
     check output.len == 1
     check output[0] == $staticteaVersion
+
+  test "main help":
+    var stream = newStringStream()
+    let rc = main(@["-h"], stream)
+    check rc == 0
+    let output = readAndClose(stream)
+    check output.len > 10
+    let expected = """
+NAME
+     statictea - combines a template with data to produce a result
+
+SYNOPSIS
+     statictea [-h] [-n] [-v] [-u] [-s=server.json] [-j=shared.json] [-t=template.html]
+       [-p="pre post"] [-r=result.html]"""
+
+    let firstFew = output[0..5].join("\n")
+    if firstFew != expected:
+      echo "----got----"
+      echo output[0..5].join("\n")
+      echo "----expected----"
+      echo expected
+      echo "----"
+      fail()
