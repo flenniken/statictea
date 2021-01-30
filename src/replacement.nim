99905e148ccfb2d72dcfa1b5500c1b1d7c0ab234
Steve Flenniken <steve.flenniken@gmail.com>
2021-01-29 17:12:17 -0800

    Remove unused replaceLine.

--- src/replacement.nim
+++ src/replacement.nim
@@ -117,57 +117,6 @@ type
     tempFile*: TempFile
     stream*: Stream
 
-proc replaceLine(env: var Env, compiledMatchers: CompiledMatchers,
-                  variables: Variables, lineNum: int, line: string, stream: Stream) {.tpub.} =
-  ## Replace the variable content in the line and output to the given
-  ## stream.
-  var pos = 0
-  var nextPos: int
-
-  while true:
-    # Get the text before the variable including the left bracket.
-    let beforeVarO = getMatches(compiledMatchers.leftBracketMatcher,
-                                line, pos)
-    if not beforeVarO.isSome:
-      # Output the rest of the line as is.
-      stream.write(line[pos .. ^1])
-      break
-    let beforeVar = beforeVarO.get()
-
-    # Match the variable. It matches leading and trailing whitespace.
-    let variableO = getMatches(compiledMatchers.variableMatcher,
-                                line, pos + beforeVar.length)
-    if not variableO.isSome:
-      nextPos = pos + beforeVar.length
-      stream.write(line[pos ..< nextPos])
-      pos = nextPos
-      continue
-    let variable = variableO.get()
-    let (whitespace, nameSpace, varName) = variable.get3Groups()
-
-    # Check that the variable ends with a right bracket.
-    nextPos = pos + beforeVar.length + variable.length
-    if nextPos == line.len or line[nextPos] != '}':
-      stream.write(line[pos ..< nextPos])
-      pos = nextPos
-      continue
-
-    # Look up the variable's value.
-    let valueO = getVariable(variables, namespace, varName)
-    if not isSome(valueO):
-      env.warn(lineNum, wMissingReplacementVar, namespace, varName)
-      nextPos = pos + beforeVar.length + variable.length + 1
-      stream.write(line[pos ..< nextPos])
-      pos = nextPos
-      continue
-    let value = valueO.get()
-
-    # Write out the text before the variable and the variable's value.
-    stream.write(line[pos ..< (pos + beforeVar.length - 1)])
-    stream.write($value)
-
-    pos = pos + beforeVar.length + variable.length + 1
-
 proc getTempFileStream(): Option[TempFileStream] {.tpub.} =
   ## Get a temporary file and an associated stream.
 
--- tests/test_replacement.nim
+++ tests/test_replacement.nim
@@ -4,7 +4,6 @@ import env
 import replacement
 import matches
 import variables
-import streams
 import options
 import tempFile
 import readlines
@@ -35,29 +34,6 @@ proc testTempSegments(templateContent: string, command: string = "nextline", rep
 
   result = env.readCloseDeleteCompare(eLogLines, eErrLines, eOutLines, eResultLines)
 
-proc testReplaceLine(line: string,
-    eLogLines: seq[string] = @[],
-    eErrLines: seq[string] = @[],
-    eOutLines: seq[string] = @[],
-    eResultLines: seq[string] = @[],
-  ): bool =
-
-  var env = openEnvTest("_testReplaceLine.log")
-
-  var stream = newStringStream()
-  let compiledMatchers = getCompiledMatchers()
-  let variables = getTestVariables()
-  let lineNum = 1
-  replaceLine(env, compiledMatchers, variables, lineNum, line, stream)
-
-  result = env.readCloseDeleteCompare(eLogLines, eErrLines, eOutLines)
-
-  let resultLines = stream.readAndClose()
-  if not expectedItems("result lines", resultLines, eResultLines):
-    echo "    : 0123456789 123456789 123456789 123456789 123456789 123456789 123456789"
-    echo "Line: " & line
-    result = false
-
 suite "replacement":
 
   # s.test = "hello"
@@ -65,67 +41,6 @@ suite "replacement":
   # five = 5
   # t.five = 5
   # g.aboutfive = 5.11
-
-  test "replaceLine empty":
-    let line = ""
-    check testReplaceLine(line)
-
-  test "replaceLine no vars":
-    let line = "this is a test."
-    let eResultLines = @["this is a test."]
-    check testReplaceLine(line, eResultLines = eResultLines)
-
-  test "replaceLine one var":
-    let line = "this is a test {s.test}."
-    let eResultLines = @["this is a test hello."]
-    check testReplaceLine(line, eResultLines = eResultLines)
-
-  test "replaceLine two vars":
-    let line = "this {s.test} is a test {s.test}."
-    let eResultLines = @["this hello is a test hello."]
-    check testReplaceLine(line, eResultLines = eResultLines)
-
-  test "replaceLine hello":
-    let line = "{s.test}"
-    let eResultLines = @["hello"]
-    check testReplaceLine(line, eResultLines = eResultLines)
-
-  test "replaceLine { no var":
-    let line = "{4s.test}"
-    let eResultLines = @["{4s.test}"]
-    check testReplaceLine(line, eResultLines = eResultLines)
-
-  test "replaceLine no }":
-    let line = "{s.test"
-    let eResultLines = @["{s.test"]
-    check testReplaceLine(line, eResultLines = eResultLines)
-
-  test "replaceLine missing var":
-    let line = "{s.missing}"
-    let eResultLines = @["{s.missing}"]
-    let eErrLines = @["template.html(1): w58: The replacement variable doesn't exist: s.missing.\n"]
-    check testReplaceLine(line, eErrLines = eErrLines, eResultLines = eResultLines)
-
-  test "replaceLine multiple vars":
-    let line = "{five}{s.missing}{h.test}"
-    let eResultLines = @["5{s.missing}there"]
-    let eErrLines = @["template.html(1): w58: The replacement variable doesn't exist: s.missing.\n"]
-    check testReplaceLine(line, eErrLines = eErrLines, eResultLines = eResultLines)
-
-  test "replaceLine var space before":
-    let line = "{ s.test}"
-    let eResultLines = @["hello"]
-    check testReplaceLine(line, eResultLines = eResultLines)
-
-  test "replaceLine var space after":
-    let line = "{s.test }"
-    let eResultLines = @["hello"]
-    check testReplaceLine(line, eResultLines = eResultLines)
-
-  test "replaceLine var lots of space":
-    let line = "{        s.test       }"
-    let eResultLines = @["hello"]
-    check testReplaceLine(line, eResultLines = eResultLines)
 
   test "getTempFileStream":
     let tempFileStreamO = getTempFileStream()
