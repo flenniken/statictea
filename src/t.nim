139f3d81827a70781b60367285d23b416394a872
Steve Flenniken <steve.flenniken@gmail.com>
2022-02-19 12:45:38 -0800

    Parse command line function tests.

--- src/t.nim
+++ src/t.nim
@@ -1,6 +1,8 @@
 ## Private module for experimenting.
 
-let str = "abc"
-echo str[0 .. 0]
-echo str[0 .. 1]
-echo str[0 .. 2]
+import std/os
+import std/strutils
+
+let count = paramCount() + 1
+for ix in 0 .. count - 1:
+  echo "$1: $2" % [$ix, paramStr(ix)]
